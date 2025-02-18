612ebd807964046abeef12dfba3d71fdf6558934
binf01-Biba <arliansel2@gmail.com>
2025-02-18 12:25:20 +0100

    primo caricamento

---
+++ src/tpc_biba/MainServer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpc_biba;
+
+
+public class MainServer {
+    public static void main(String[] args) {
+        Server s = new Server(2000);
+    }
+}
+
---
+++ src/tpc_biba/Server.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpc_biba;
+
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Server {
+    private ServerSocket serverSocket; 
+    private Socket clientSocket; 
+    private int porta;
+    
+    
+   
+   
+    public Server(int porta) {
+        this.porta = porta;
+        try {
+            serverSocket = new ServerSocket(porta);
+            System.out.println("Server in ascolto sulla porta: " + porta);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Server non in ascolto");
+        }
+    }
+    
+    public Socket attendi(){
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return clientSocket;
+    }
+    
+   
+}
